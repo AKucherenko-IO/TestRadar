039322e4508617cee537c09e6c722ff107742560
Andrei Kucherenko <aka2676@gmail.com>
2018-11-25 12:37:40 +0700

    Draw in class ShapeView

--- TestRadar/ShapeView.swift
+++ TestRadar/ShapeView.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+@IBDesignable
+
+
 class ShapeView: UIView {
 
     /*
@@ -17,76 +20,70 @@ class ShapeView: UIView {
         // Drawing code
     }
     */
-    var currentShapeType: Int = 0
-    
-    init(frame: CGRect, shape: Int) {
-        super.init(frame: frame)
-        currentShapeType = shape
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    let currentShapeType: Int = 2
+    let lineWidht: CGFloat = 2
     
     override func draw(_ rect: CGRect) {
+        
+        let center = CGPoint(x: self.frame.size.width / 2.0, y:self.frame.size.height / 2.0)
+        print ("Circle: center: \(center) frame: \(self.frame.size.width) \(self.frame.size.height)")
+        let radius: CGFloat = center.x < center.y ? center.x - lineWidht : center.y - lineWidht
+        
         switch currentShapeType {
         case 0: drawLines()
         case 1: drawRectangle()
-        case 2: drawCircle()
+        case 2: drawCircle(center: center, radius: radius)
         default: print("default")
         }
     }
     
     func drawLines() {
-        //1
-        let ctx = UIGraphicsGetCurrentContext()
-        
-        //2
-        (ctx ?? nil)!.beginPath()
-        
-        ctx?.move(to: CGPoint(x:20.0, y:20.0))
-        ctx?.move(to: CGPoint(x:20.0, y:20.0))
-        ctx?.move(to: CGPoint(x:250.0, y:100.0))
-        ctx?.move(to: CGPoint(x:100.0, y:200.0))
-        ctx?.setLineWidth(5)
-        
-        //3
-        ctx?.closePath()
-        ctx?.strokePath()
+//        //1
+//        let ctx = UIGraphicsGetCurrentContext()
+//
+//        //2
+//        (ctx ?? nil)!.beginPath()
+//
+//        ctx?.move(to: CGPoint(x:20.0, y:20.0))
+//        ctx?.move(to: CGPoint(x:20.0, y:20.0))
+//        ctx?.move(to: CGPoint(x:250.0, y:100.0))
+//        ctx?.move(to: CGPoint(x:100.0, y:200.0))
+//        ctx?.setLineWidth(lineWidht)
+//
+//        //3
+//        ctx?.closePath()
+//        ctx?.strokePath()
     }
     
     func drawRectangle() {
-        let center = CGPoint(x: self.frame.size.width / 2.0, y: self.frame.size.height / 2.0)
-        let rectangleWidth:CGFloat = 100.0
-        let rectangleHeight:CGFloat = 100.0
-        let ctx = UIGraphicsGetCurrentContext()
-        
-        //4
-        ctx?.addRect(CGRect(x: (center.x - (0.5 * rectangleWidth)), y: (center.y - (0.5 * rectangleHeight)), width: rectangleWidth, height: rectangleHeight))
-        
-        ctx?.setLineWidth(10)
-        ctx?.setStrokeColor(red: 60, green: 30, blue: 50, alpha: 2)
-        ctx?.strokePath()
-        
-        //5
-        ctx?.setFillColor(red: 100, green: 100, blue: 50, alpha: 1)
-        ctx?.addRect(CGRect(x: (center.x - (0.5 * rectangleWidth)), y: (center.y - (0.5 * rectangleHeight)), width: rectangleWidth, height: rectangleHeight))
-    
-        ctx?.fillPath()
-        
+//        let center = CGPoint(x: self.frame.size.width / 2.0, y: self.frame.size.height / 2.0)
+//        let rectangleWidth:CGFloat = 100.0
+//        let rectangleHeight:CGFloat = 100.0
+//        let ctx = UIGraphicsGetCurrentContext()
+//
+//        //4
+//        ctx?.addRect(CGRect(x: (center.x - (0.5 * rectangleWidth)), y: (center.y - (0.5 * rectangleHeight)), width: rectangleWidth, height: rectangleHeight))
+//
+//        ctx?.setLineWidth(10)
+//        ctx?.setStrokeColor(red: 60, green: 30, blue: 50, alpha: 2)
+//        ctx?.strokePath()
+//
+//        //5
+//        ctx?.setFillColor(red: 100, green: 100, blue: 50, alpha: 1)
+//        ctx?.addRect(CGRect(x: (center.x - (0.5 * rectangleWidth)), y: (center.y - (0.5 * rectangleHeight)), width: rectangleWidth, height: rectangleHeight))
+//
+//        ctx?.fillPath()
+//
     }
     
-    func drawCircle() {
-        let center = CGPoint(x: self.frame.size.width / 2.0, y:self.frame.size.height / 2.0)
+    func drawCircle(center: CGPoint, radius: CGFloat) {
+        
         let ctx = UIGraphicsGetCurrentContext()
         ctx?.beginPath()
         
         //6
-        ctx?.setLineWidth(5)
+        ctx?.setLineWidth(lineWidht)
 
-        let x:CGFloat = center.x
-        let y:CGFloat = center.y
-        let radius: CGFloat = 100.0
         let endAngle: CGFloat = CGFloat(2 * Double.pi)
         ctx?.addArc(center: center, radius: radius, startAngle: 0, endAngle: endAngle, clockwise: true)
         ctx?.strokePath()
--- TestRadar/ViewController.swift
+++ TestRadar/ViewController.swift
@@ -13,146 +13,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print ("DidApear")
-        let screenWidth = UIScreen.main.bounds.width
-        let screenHeight =  UIScreen.main.bounds.height
-        print("width:\(screenWidth) height:\(screenHeight)")
-        
-        let rectSize = screenWidth > screenHeight ? screenHeight : screenWidth
-        let viewRectX: CGFloat = 0;
-        let viewRectY: CGFloat = screenHeight/2.0 - rectSize/2.0
-        //        let center = CGPoint(x:  screenWidth/2.0, y: screenHeight/2.0)
-        let myView = ShapeView(frame: CGRect(x: viewRectX, y: viewRectY, width: rectSize, height: rectSize ), shape: 2)
-        myView.backgroundColor = UIColor.cyan
-        self.view.addSubview(myView)
-        
-//        // Do any additional setup after loading the view, typically from a nib.
-//        // Drawing code
-//        //[super drawRect:rect];
 //
-////        NSLog(@"drawRect %@", NSStringFromCGRect(rect));
+//        print ("DidApear")
+//        let screenWidth = UIScreen.main.bounds.width
+//        let screenHeight =  UIScreen.main.bounds.height
+//        print("width:\(screenWidth) height:\(screenHeight)")
 //
-//        CGContextRef context = UIGraphicsGetCurrentContext();
-//
-//        /*
-//
-//         CGRect square1 = CGRectMake(100, 100, 100, 100);
-//         CGRect square2 = CGRectMake(200, 200, 100, 100);
-//         CGRect square3 = CGRectMake(300, 300, 100, 100);
-//
-//
-//         //CGContextSetFillColorWithColor(context, [UIColor redColor].CGColor);
-//         CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
-//
-//         CGContextAddRect(context, square1);
-//         CGContextAddRect(context, square2);
-//         CGContextAddRect(context, square3);
-//
-//         //CGContextFillPath(context);
-//         CGContextStrokePath(context);
-//
-//         CGContextSetFillColorWithColor(context, [UIColor greenColor].CGColor);
-//
-//         CGContextAddEllipseInRect(context, square1);
-//         CGContextAddEllipseInRect(context, square2);
-//         CGContextAddEllipseInRect(context, square3);
-//
-//         CGContextFillPath(context);
-//
-//         CGContextSetStrokeColorWithColor(context, [UIColor blueColor].CGColor);
-//
-//         CGContextSetLineWidth(context, 1.f);
-//         CGContextSetLineCap(context, kCGLineCapRound);
-//
-//         CGContextMoveToPoint(context, CGRectGetMinX(square1), CGRectGetMaxY(square1));
-//         CGContextAddLineToPoint(context, CGRectGetMinX(square3), CGRectGetMaxY(square3));
-//
-//         CGContextMoveToPoint(context, CGRectGetMaxX(square3), CGRectGetMinY(square3));
-//         CGContextAddLineToPoint(context, CGRectGetMaxX(square1), CGRectGetMinY(square1));
-//
-//         CGContextStrokePath(context);
-//
-//         CGContextSetStrokeColorWithColor(context, [UIColor grayColor].CGColor);
-//
-//         CGContextMoveToPoint(context, CGRectGetMinX(square1), CGRectGetMaxY(square1));
-//         CGContextAddArc(context, CGRectGetMaxX(square1), CGRectGetMaxY(square1), CGRectGetWidth(square1), M_PI, 0, YES);
-//
-//         CGContextMoveToPoint(context, CGRectGetMaxX(square3), CGRectGetMinY(square3));
-//         CGContextAddArc(context, CGRectGetMinX(square3), CGRectGetMinY(square3), CGRectGetWidth(square3), 0, M_PI, YES);
-//
-//         CGContextStrokePath(context);
-//
-//         NSString* text = @"test";
-//
-//         UIFont* font = [UIFont systemFontOfSize:14.f];
-//
-//         NSShadow* shadow = [[NSShadow alloc] init];
-//         shadow.shadowOffset = CGSizeMake(1, 1);
-//         shadow.shadowColor = [UIColor whiteColor];
-//         shadow.shadowBlurRadius = 0.5;
-//
-//         NSDictionary* attirbutes =
-//         [NSDictionary dictionaryWithObjectsAndKeys:
-//         [UIColor grayColor],   NSForegroundColorAttributeName,
-//         font,                  NSFontAttributeName,
-//         shadow,                NSShadowAttributeName, nil];
-//
-//         CGSize textSize = [text sizeWithAttributes:attirbutes];
-//
-//         CGRect textRect = CGRectMake(CGRectGetMidX(square2) - textSize.width / 2,
-//         CGRectGetMidY(square2) - textSize.height / 2,
-//         textSize.width, textSize.height);
-//
-//         textRect = CGRectIntegral(textRect);
-//
-//         [text drawInRect:textRect withAttributes:attirbutes];
-//
-//         //[text drawAtPoint:CGPointMake(CGRectGetMidX(square2) - textSize.width / 2, CGRectGetMidY(square2) - textSize.height / 2) withAttributes:attirbutes];
-//         */
-//
-//        CGFloat offset = 50.f;
-//        CGFloat borderWidth = 4.f;
-//
-//        CGFloat maxBoardSize = MIN(CGRectGetWidth(rect) - offset * 2 - borderWidth * 2,
-//                                   CGRectGetHeight(rect) - offset * 2 - borderWidth *2);
-//
-//        int cellSize = (int)maxBoardSize / 8;
-//        int boardSize = cellSize * 8;
-//
-//        CGRect boardRect = CGRectMake((CGRectGetWidth(rect) - boardSize) / 2,
-//                                      (CGRectGetHeight(rect) - boardSize) / 2,
-//                                      boardSize, boardSize);
-//
-//        boardRect = CGRectIntegral(boardRect);
-//
-//        for (int i = 0; i < 8; i++) {
-//            for (int j = 0; j < 8; j++) {
-//                if (i % 2 != j % 2) {
-//                    CGRect cellRect =
-//                        CGRectMake(CGRectGetMinX(boardRect) + i * cellSize,
-//                                   CGRectGetMinY(boardRect) + j * cellSize,
-//                                   cellSize, cellSize);
-//
-//                    CGContextAddRect(context, cellRect);
-//                }
-//            }
-//        }
-//
-//
-//        CGContextSetFillColorWithColor(context, [UIColor grayColor].CGColor);
-//
-//        CGContextFillPath(context);
-//
-//        CGContextSetStrokeColorWithColor(context, [UIColor grayColor].CGColor);
-//
-//        CGContextAddRect(context, boardRect);
-//
-//        CGContextSetLineWidth(context, borderWidth);
-//
-//        CGContextStrokePath(context);
+//        let rectSize = screenWidth > screenHeight ? screenHeight : screenWidth
+//        let viewRectX: CGFloat = 0;
+//        let viewRectY: CGFloat = screenHeight/2.0 - rectSize/2.0
+//        //        let center = CGPoint(x:  screenWidth/2.0, y: screenHeight/2.0)
+//        let myView = ShapeView(frame: CGRect(x: viewRectX, y: viewRectY, width: rectSize, height: rectSize ), shape: 2)
+//        myView.backgroundColor = UIColor.cyan
+//        self.view.addSubview(myView)
     }
 
 
 }
-
