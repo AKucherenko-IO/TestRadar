bda3a85df78742467b49fffc85396e6abd6c4078
Andrei Kucherenko <aka2676@gmail.com>
2018-12-01 16:02:20 +0700

    generate new plane in permitted area

--- TestRadar/AKRadarView.swift
+++ TestRadar/AKRadarView.swift
@@ -15,8 +15,8 @@ class AKRadarView: UIView {
     //MARK: Define parameters
     let imageNamePlane1 = "plane-1"
     let lineWidht: CGFloat = 2
-    let numberOfCircles: Int = 3
-    let numberOfPlanes: Int = 20
+    let numberOfCircles: Int = 2
+    let numberOfPlanes: Int = 100
     var dimensionPlane: CGFloat = 20
     let dimensionDelta: CGFloat = 5
     var availablePositions: Int = 8
@@ -24,7 +24,7 @@ class AKRadarView: UIView {
     let circleBorderColor: UIColor = UIColor.black
     
     var allRandomPlanesInArea = [[AKPlane]]()
-    var radiusRadarArray: [CGFloat] = []
+    var inerAreaArray: [(CGFloat, CGFloat)] = []
     var planeRadius: CGFloat = 0
     var neighbors: Int = 0
     var isOverlayPlane:Bool = false
@@ -39,11 +39,11 @@ class AKRadarView: UIView {
         backgroundColor = .white
         let screenWidth = UIScreen.main.bounds.width
         let screenHeight =  UIScreen.main.bounds.height
-        
+        print ("Screen size: width=\(screenWidth)\theight:\(screenHeight)")
         let rectSize = screenWidth > screenHeight ? screenHeight : screenWidth
         let viewRectX: CGFloat = 0;
         let viewRectY: CGFloat = screenHeight/2.0 - rectSize/2.0
-
+        print ("viewY: \(viewRectY) = \(screenHeight)/2 -\(rectSize)/2")
         self.frame = CGRect(x: viewRectX, y: viewRectY, width:rectSize, height:rectSize)
         
     }
@@ -56,30 +56,48 @@ class AKRadarView: UIView {
         let center = CGPoint(x: halfFrameSize, y:halfFrameSize)
         var radius: CGFloat = halfFrameSize - lineWidht
         let radiusDelta: CGFloat = radius / CGFloat(numberOfCircles)
+        planeRadius = sqrt(2)/2 * dimensionPlane
         
-        //MARK: -  Generate Radar Circles
+        let inerAreaSize = radiusDelta - (2 * planeRadius)
+        dimensionPlane = radiusDelta > dimensionPlane ? dimensionPlane : radiusDelta/2
+        //MARK: Check Center
+        print ("Frame Size: \(frameSize) \t \(halfFrameSize)")
+        print("Center: \(center)\tareaSize:\(inerAreaSize)")
+        
+        
+        //MARK: -  Generate Radar Circles and svae innerArea for planes
         for _ in 0..<numberOfCircles {
             
             drawCircle(center: center, radius: radius)
-            radiusRadarArray.append(radius)
+            let externalRadius = radius - planeRadius
             radius -= radiusDelta
+            let internalRadius = radius != 0 ? radius + planeRadius : 0
+            let inerArea = (externalRadius,internalRadius)
             let randomPlanesInArea = [AKPlane] ()
+            inerAreaArray.append(inerArea)
             allRandomPlanesInArea.append(randomPlanesInArea)
 
         }
-        
-        dimensionPlane = radiusDelta > dimensionPlane ? dimensionPlane : radiusDelta/2
-        
-        planeRadius = sqrt(2)/2 * dimensionPlane
+
 
         //MARK: - Generate Planes locations
         var i: Int = 1;
   
         while  (i <= numberOfPlanes) {
-            
-            let planePositionX = CGFloat.random(in: 0..<frameSize)
-            let planePositionY = CGFloat.random(in: 0..<frameSize)
-            let flag = checkPosition(x: planePositionX, y: planePositionY, center: center, delta: radiusDelta)
+            let randomDeltaRadius = CGFloat.random(in: 0..<inerAreaSize)
+//                CGFloat(0)
+//                CGFloat.random(in: 0..<inerAreaSize)
+            if (randomDeltaRadius == 0) {
+                print ("randomDeltaRadius:\(randomDeltaRadius)")
+            }
+            let randomCircle = Int.random(in: 0..<numberOfCircles)
+            let randomAngle = Double.random(in: 0...360)
+            let randomRadius =  Double(randomDeltaRadius + inerAreaArray[randomCircle].1)
+            let planePositionX = center.x + CGFloat(randomRadius * sin(randomAngle)) - dimensionPlane / 2
+            let planePositionY = center.y + CGFloat(randomRadius * cos(randomAngle)) - dimensionPlane / 2
+//            print ("Param: randomCircle:\(randomCircle)\trandomDeltaRadius:\(randomDeltaRadius)\trandomAngle:\(randomAngle)\trandomRadius:\(randomRadius)  " )
+//            let flag = checkPosition(x: planePositionX, y: planePositionY, center: center, delta: radiusDelta)
+            let flag = checkIntersectsInArea(x: planePositionX, y: planePositionY, sector: randomCircle, positionRadius: CGFloat(randomRadius), delta: inerAreaSize)
             
             if (flag) {
                 
@@ -105,41 +123,41 @@ class AKRadarView: UIView {
     }
 
     
-    func checkPosition(x: CGFloat, y: CGFloat, center: CGPoint, delta: CGFloat) -> Bool {
-        
-        var isInsideArea = false
-        var radarBorderIn = CGFloat(0)
-        let halfSize = dimensionPlane / 2.0
-        let planeCenterX = x + halfSize
-        let planeCenterY = y + halfSize
-        let xDelta = planeCenterX - center.x
-        let yDelta = planeCenterY - center.y
-        let conditionRule = pow(xDelta,2) + pow(yDelta,2)
-        let positionRadius = sqrt(conditionRule)
-        var sector = Int (abs((radiusRadarArray[0] - positionRadius)) / delta)
-        
-        sector = (positionRadius != 0 ) ? sector :  radiusRadarArray.count - 1
-        
-        let indent = lineWidht + dimensionDelta
-        let correctionOut = radiusRadarArray[sector] - planeRadius - indent
-        let radarBorderOut  = pow(correctionOut,2)
-        
-        if (sector < radiusRadarArray.count - 1) {
-        
-            let correctionIn = radiusRadarArray[sector+1] + planeRadius + indent
-            radarBorderIn  = pow(correctionIn,2)
-        
-        }
+//    func checkPosition(x: CGFloat, y: CGFloat, center: CGPoint, delta: CGFloat) -> Bool {
+//        
+//        var isInsideArea = false
+//        var radarBorderIn = CGFloat(0)
+//        let halfSize = dimensionPlane / 2.0
+//        let planeCenterX = x + halfSize
+//        let planeCenterY = y + halfSize
+//        let xDelta = planeCenterX - center.x
+//        let yDelta = planeCenterY - center.y
+//        let conditionRule = pow(xDelta,2) + pow(yDelta,2)
+//        let positionRadius = sqrt(conditionRule)
+//        var sector = Int (abs((inerAreaArray[0] - positionRadius)) / delta)
+//
+//        sector = (positionRadius != 0 ) ? sector :  radiusRadarArray.count - 1
+//
+//        let indent = lineWidht + dimensionDelta
+//        let correctionOut = radiusRadarArray[sector] - planeRadius - indent
+//        let radarBorderOut  = pow(correctionOut,2)
+//
+//        if (sector < radiusRadarArray.count - 1) {
+//
+//            let correctionIn = radiusRadarArray[sector+1] + planeRadius + indent
+//            radarBorderIn  = pow(correctionIn,2)
+//
+//        }
 
-        if (radarBorderOut > conditionRule)&&( conditionRule >= radarBorderIn) {
-            
-            isInsideArea = checkIntersectsInArea(x: x, y: y, sector: sector, positionRadius: positionRadius, delta: delta)
-
-        }
-        
-        return  isInsideArea
-        
-    }
+//        if (radarBorderOut > conditionRule)&&( conditionRule >= radarBorderIn) {
+//
+//            isInsideArea = checkIntersectsInArea(x: x, y: y, sector: sector, positionRadius: positionRadius, delta: delta)
+//
+//        }
+//
+//        return  isInsideArea
+//
+//    }
     //MARK:  Check Intersections with saved objects
     func checkIntersectsInArea(x: CGFloat, y: CGFloat, sector: Int, positionRadius: CGFloat, delta: CGFloat) -> Bool {
         
@@ -181,11 +199,11 @@ class AKRadarView: UIView {
                 }
             }
             
-            if (positionRadius + 3*planeRadius) >= radiusRadarArray[sector] {
+            if (positionRadius + 2*planeRadius) >= inerAreaArray[sector].0 {
                 randomPlane.neighbors += 1
                 neighbors += 1
             }
-            if ((positionRadius - 3*planeRadius) <= radiusRadarArray[sector]-delta) {
+            if ((positionRadius - 2*planeRadius) <= inerAreaArray[sector].1) {
                 randomPlane.neighbors += 1
                 neighbors += 1
             }
